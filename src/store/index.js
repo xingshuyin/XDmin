f33e328a3ab57a0ed3af1cb3e9d475da6df934c1
xing <xingshuyin@outlook.com>
2022-07-31 16:42:34 +0800

    1

---
+++ src/store/index.js
@@ -0,0 +1,12 @@
+import { createStore } from 'vuex'
+
+export default createStore({
+    state() {  //定义状态
+        return {
+            verift: false,
+        }
+    },
+    mutations: {
+        
+    }
+})
